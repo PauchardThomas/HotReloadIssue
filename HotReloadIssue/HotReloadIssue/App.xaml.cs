f69ab097b0b670bcba9af208f8da04e1c12f7ef6
tpauchard <t.pauchard@hokoha.com>
2019-12-31 15:34:43 +0100

    Ajoutez des fichiers projet.

---
+++ HotReloadIssue/HotReloadIssue/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HotReloadIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+#if DEBUG
+            HotReloader.Current.Run(this);
+#endif
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
